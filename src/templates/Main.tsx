de2be01a5c0ec0d4ef5fc3113706ead57acf9cd6
Juan Jose Cardona <joscardona@icloud.com>
2021-02-23 23:54:05 +0000

    fixes padding

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -16,13 +16,13 @@ const Main = (props: IMainProps) => (
 
     <div className="max-w-screen-md mx-auto">
       <div className="border-b border-gray-300">
-        <div className="pt-16 pb-8">
+        <div className="pt-16 px-8 md:px-0 pb-8">
           <div className="font-semibold text-3xl text-gray-900">{Config.title}</div>
           <div className="text-xl">{Config.description}</div>
         </div>
         <div>
           <Navbar>
-            <li className="mr-6">
+            <li className="mx-6">
               <Link href="/">
                 <a>Home</a>
               </Link>
